5a7f80ef55160d2a3ca50a3f9553f10b69a61a38
AlexNuzhdov <alexnuzhdov90@gmail.com>
2022-10-01 00:18:27 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import user from 'components/Profile/user.json';
-import {Profile} from 'components/Profile/Profile';
-import {Statistics} from 'components/Statistics/Statistics';
+import {Profile} from '../components/Profile/Profile';
+import {Statistics} from '../components/Statistics/Statistics';
 import data from 'components/Statistics/data.json'
-import {FriendList} from 'components/FriendList/FriendList';
+import {FriendList} from '../components/FriendList/FriendList';
 import friends from 'components/FriendList/friends.json';
-import {TransactionHistory} from 'components/TransactionHistory/TransactionHistory';
+import {TransactionHistory} from '../components/TransactionHistory/TransactionHistory';
 import transactions from 'components/TransactionHistory/transactions.json';
 
 
